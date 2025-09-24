cc3c8f09b32e7498e5cdf2adea28bffcea9cc1d2
DeBoiCruzyy <cruz.arreola.heat@gmail.com>
2025-09-23 18:29:37 -0700

    Initial Commit

---
+++ FirstAppFix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirstAppFix
+//
+//  Created by Cruz Arreola on 9/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirstAppFix/FirstAppFixApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppFixApp.swift
+//  FirstAppFix
+//
+//  Created by Cruz Arreola on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppFixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
