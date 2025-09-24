05728a409a6b0eed5986da8ffc5ba7978008659f
DeBoiCruzyy <cruz.arreola.heat@gmail.com>
2025-09-23 18:53:42 -0700

    Only commit of fixed Hello app

--- FirstAppFix/ContentView.swift
+++ FirstAppFix/ContentView.swift
@@ -7,17 +7,26 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+struct ContentView: View {     // creating view screen
+    @State private var helloWorld = false   // making a boolean variable
+        /* @State makes the variable changeable for the button, when
+         changed to "true" the pop-up will appear */
+    
+    var body: some View{   // this "body" section contains all the UI
+            Button("Hello"){  // creating a button that says "Hello"
+                helloWorld = true   // this will trigger if button is pressed
+            }
+            .alert("Hello World", isPresented: $helloWorld){    /*alert will pop up
+                                                                 when helloWorld is true...
+                                                                 $ allows the variable to
+                                                                 be updated*/
+                Button("Ok"){               // creating button within the alert
+                    helloWorld = false      
+                }
+            }
     }
 }
+
 
 #Preview {
     ContentView()
